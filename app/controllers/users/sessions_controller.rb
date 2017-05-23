d6f4e2e8df6bd02aca5328c17d017d52909564b0
Peter Quinones <peter.j.quinones@vanderbilt.edu>
2017-05-22 22:52:53 -0500

    Added login modal (still need to update with logo)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,9 +7,21 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    resource = warden.authenticate!(:scope => resource_name, :recall => 'users/sessions#failure')
+    sign_in_and_redirect(resource_name, resource)
+  end
+
+  def sign_in_and_redirect(resource_or_scope, resource=nil)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    resource ||= resource_or_scope
+    sign_in(scope, resource) unless warden.user(scope) == resource
+    redirect_to root_path
+  end
+
+  def failure
+    render :action => 'login_failure.js.erb'
+  end
 
   # DELETE /resource/sign_out
   # def destroy
