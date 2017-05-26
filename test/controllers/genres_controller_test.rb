4883b4902f18163b6584fc66c1562e00cde467ff
Peter Quinones <peter.j.quinones@vanderbilt.edu>
2017-05-26 17:04:18 -0500

    Removed 2 tests for deleted genre routes

--- test/controllers/genres_controller_test.rb
+++ test/controllers/genres_controller_test.rb
@@ -28,16 +28,6 @@ class GenresControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_genre_url(@genre)
-    assert_response :success
-  end
-
-  test "should update genre" do
-    patch genre_url(@genre), params: { genre: {  } }
-    assert_redirected_to genre_url(@genre)
-  end
-
   # test "should destroy genre" do
   #   assert_difference('Genre.count', -1) do
   #     delete genre_url(@genre)
