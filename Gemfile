20486f2755768ef8afdbc1ed46ac961b3abeb563
Peter Quinones <peter.j.quinones@vanderbilt.edu>
2017-06-02 00:24:12 -0500

    Added tether

--- Gemfile
+++ Gemfile
@@ -68,6 +68,9 @@ gem 'material_icons'
 #Glyphicons gem
 gem 'glyphicons-rails'
 
+#Tether gem
+gem 'tether-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,5 @@ module Mybandsource
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.assets.precompile += %w(*.js)
   end
 end
