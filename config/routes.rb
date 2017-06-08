f0ff8f2b3a0e3994c2f67875765d26f014616ca1
Peter Quinones <peter.j.quinones@vanderbilt.edu>
2017-06-07 23:03:59 -0500

    Put in correct share icon for profile page

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
     post '/share/review/:id', to: 'reviews#share'
     post '/share/reviews/:review_id/response', to: 'responses#share'
   end
-  
+
   #User relationship create and destroy routes
   post '/userrelationship/:id', to: 'user_relationships#create'
   delete '/userrelationship/:id', to: 'user_relationships#destroy'
