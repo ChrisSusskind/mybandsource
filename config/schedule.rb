519d3465a34fd4fb0e68b940dcf3d7775df55387
Peter Quinones <peter.j.quinones@vanderbilt.edu>
2017-06-15 12:16:58 -0500

    Trying to add schedule.rb back in

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,3 +19,9 @@
 
 # Learn more: http://github.com/javan/whenever
 
+set :environment, "production"
+set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
+
+every 6.hours do
+  rake "calculate_average_rating_and_counts"
+end
