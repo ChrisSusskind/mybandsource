0f8fa242de82ad8508d21852bba0a830f9e051ee
Peter Quinones <peter.j.quinones@vanderbilt.edu>
2017-06-22 16:31:58 -0400

    Created static branch to show Mike and Chris

--- app/assets/javascripts/components/_artist_search_component.js.jsx
+++ app/assets/javascripts/components/_artist_search_component.js.jsx
@@ -3,7 +3,7 @@ const ArtistSearchComponent = props => {
     return(
         <div className="text-center">
             <div className="input-group input-group-lg">
-                <input type="text" name="query" className="form-control search-bar" placeholder="Search for a band..." onChange={props.searchPath} onKeyDown={function(e){if(e.keyCode == 13){props.submitSearch()}}}/>
+                <input type="text" name="query" className="form-control search-bar" placeholder="Search Artist" onChange={props.searchPath} onKeyDown={function(e){if(e.keyCode == 13){props.submitSearch()}}}/>
             </div>
             {props.showButton == true ? (
                     <button onClick={props.submitSearch} className="btn btn-danger button-home search-btn mx-auto m-3">Search</button>
