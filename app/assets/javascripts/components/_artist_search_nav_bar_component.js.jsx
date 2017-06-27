6142091222bb65d41713a4a5104fdd4d1e5790b1
Peter Quinones <peter.j.quinones@vanderbilt.edu>
2017-06-26 23:40:21 -0500

    Styled header

--- app/assets/javascripts/components/_artist_search_nav_bar_component.js.jsx
+++ app/assets/javascripts/components/_artist_search_nav_bar_component.js.jsx
@@ -5,6 +5,10 @@
 const ArtistSearchNavBarComponent = props => {
 
     return(
-        <input type="text" name="search" className="search-box" placeholder="Search Artist" onChange={props.searchPath} onKeyDown={function(e){if(e.keyCode == 13){props.submitSearch()}}}/>
+        <div>
+            <i className="material-icons md-dark search-icon" id="search-icon">search</i>
+            <input type="text" name="search" className="search-box my-auto" placeholder="Search Artist" onChange={props.searchPath} onKeyDown={function(e){if(e.keyCode == 13){props.submitSearch()}}}/>
+        </div>
+
     );
 };
