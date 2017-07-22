31da498749951d4979206c4f0a36ca7e88014974
Eric Koehler <rikola2@gmail.com>
2017-07-21 23:55:36 -0500

    Removed non-SSL images and fixed admin portal Genre display

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,10 +38,10 @@ Rails.application.configure do
   config.action_cable.mount_path = '/cable'
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.url = "wss://#{ENV['DEFAULT_HOST']}/cable"
+  config.action_cable.url = "ws://#{ENV['DEFAULT_HOST']}/cable"
   config.action_cable.allowed_request_origins = ["https://dev.mybandsource.com"]
   config.action_cable.disable_request_forgery_protection = true
-  config.web_socket_server_url = "wss://dev.mybandsource.com/cable"
+  config.web_socket_server_url = "ws://dev.mybandsource.com/cable"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
