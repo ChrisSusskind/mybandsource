63362bd38c57e661a639e418f10e9a6678387638
bennygrub <blgruber@gmail.com>
2017-09-05 14:50:40 -0700

    adding new updates to artist page and overall layout.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @fullwidth = true
     if @user.is_artist
       @artist = @user
       if user_signed_in?
