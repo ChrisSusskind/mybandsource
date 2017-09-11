f58a1d2c73bbdd14cccb7fd1ae0dbaaaf29bfea9
bennygrub <blgruber@gmail.com>
2017-09-11 12:30:22 -0700

    dropdown menus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,5 @@ class ApplicationController < ActionController::Base
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :location, :genre, :facebook_url, :itunes_url, :soundcloud_url, :spotify_url, :twitter_url, :genres_list => []])
 	end
+  
 end
