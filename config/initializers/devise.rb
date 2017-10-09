11bbe82c664e860a56f5f23ab63f988befca9b20
bennygrub <blgruber@gmail.com>
2017-10-09 12:19:59 -0700

    moving to live fb app

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,7 +247,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, '1288660244596638', 'f45ce4ef9cfe1c3bd24ba6c63c82c486'
+  config.omniauth :facebook, '243060176100394', 'c7a89e4201587c7cc688a8e751f4c98a'
 
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
